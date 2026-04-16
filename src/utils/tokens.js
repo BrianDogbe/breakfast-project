19677a0fdf09e97d63bb9f3615486d68204f3db3
Brian Dela Dogbe <dogbebrian1@gmail.com>
2026-04-16 17:13:57 +0000

    remove unused firebase and config files

--- src/utils/tokens.js
+++
@@ -1,8 +0,0 @@
-function makeToken(byteLen = 16) {
-  // Hex token (32 chars at byteLen=16)
-  const { randomBytes } = require("crypto");
-  return randomBytes(byteLen).toString("hex");
-}
-
-module.exports = { makeToken };
-
